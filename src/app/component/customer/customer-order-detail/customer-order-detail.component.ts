88ccf6d52595b4a9e62b19512641800e04d7104c
cuong le <lecuong220120@gmail.com>
2022-04-05 21:51:54 +0700

    complete customer order detail html/css

--- src/app/component/customer/customer-order-detail/customer-order-detail.component.ts
+++ src/app/component/customer/customer-order-detail/customer-order-detail.component.ts
@@ -2,6 +2,9 @@ import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {OrderDetail} from '../../../model/OrderDetail';
 import {OrderDetailService} from '../../../service/order-detail.service';
 import {OrderService} from '../../../service/order.service';
+import {Product} from '../../../model/Product';
+import {ProductService} from '../../../service/product.service';
+
 
 @Component({
   selector: 'app-customer-order-detail',
@@ -9,31 +12,56 @@ import {OrderService} from '../../../service/order.service';
   styleUrls: ['./customer-order-detail.component.scss']
 })
 export class CustomerOrderDetailComponent implements OnInit {
-  orderDetailList: OrderDetail[] = [];
+  product: Product = {};
   @Input()
-  orderDetail: OrderDetail = {};
+  orderDetailList: OrderDetail[] = [];
   @Output()
   orderDetailChange: EventEmitter<OrderDetail> = new EventEmitter<OrderDetail>();
 
   constructor(private orderDetailService: OrderDetailService,
-              private orderService: OrderService) {
+              private orderService: OrderService,
+              private productService: ProductService) {
   }
 
   ngOnInit(): void {
-    console.log(this.orderDetail);
-    console.log(this.orderDetail.product);
   }
 
-  update(): void {
-    this.orderDetailChange.emit(this.orderDetail);
+  update(orderDetail: OrderDetail): void {
+    this.productService.findById(orderDetail.product.id).subscribe(data => {
+      this.product = data;
+    });
+    if (orderDetail.orderQuantity > this.product.quantity){
+      alert('Số lượng sản phẩm không đủ!!');
+      orderDetail.orderQuantity = this.product.quantity;
+    }else{
+      this.orderDetailService.updateOrderDetail(orderDetail).subscribe();
+      alert('Thay đổi số lượng thành công!');
+    }
   }
-
-  // findOrderDetailList(): void {
-  //   this.orderDetailService.findAll().subscribe(orderDetailList => {
-  //     this.orderDetailList = orderDetailList;
-  //   });
-  // }
-
+  updateMinus(orderDetail: OrderDetail): void {
+    orderDetail.orderQuantity = orderDetail.orderQuantity - 1;
+    if (orderDetail.orderQuantity === 0){
+      if (confirm('Bạn có muốn xóa sản phẩm này ko?') === true){
+          this.deleteOrderDetail(orderDetail.id);
+      }else{
+        orderDetail.orderQuantity = 1;
+      }
+    }else{
+      this.orderDetailService.updateOrderDetail(orderDetail).subscribe();
+    }
+  }
+  updatePlus(orderDetail: OrderDetail): void {
+    orderDetail.orderQuantity = orderDetail.orderQuantity + 1;
+    this.productService.findById(orderDetail.product.id).subscribe(data => {
+      this.product = data;
+    });
+    if (orderDetail.orderQuantity > this.product.quantity){
+      alert('Số lượng sản phẩm không đủ!!');
+      orderDetail.orderQuantity = this.product.quantity;
+    }else{
+      this.orderDetailService.updateOrderDetail(orderDetail).subscribe();
+    }
+  }
   deleteOrderDetail(id: number): void {
     this.orderDetailService.deleteOrderDetail(id).subscribe(() => {
       window.location.reload();
