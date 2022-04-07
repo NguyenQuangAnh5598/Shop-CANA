14e77d427c8f017d1fb459d8f95d8e80dd0818cf
tvnam0609 <namtv060900@gmail.com>
2022-04-07 11:02:35 +0700

    #17/complete css

--- src/app/component/admin/admin-revenue/admin-revenue.component.ts
+++ src/app/component/admin/admin-revenue/admin-revenue.component.ts
@@ -79,4 +79,20 @@ export class AdminRevenueComponent implements OnInit {
     });
   }
 
+  onButtonClick($e): void {
+    console.log($e);
+    const clickedElement = $e.target ;
+    console.log(clickedElement);
+    console.log($e.target.value);
+    if (clickedElement.nodeName === 'A') {
+      const isActivated = clickedElement.parentElement.querySelector('.active');
+      if (isActivated) {
+        isActivated.classList.remove('active');
+      }
+
+      // clickedElement.className = clickedElement.append(' active');
+      clickedElement.className += ' active';
+    }
+  }
+
 }
